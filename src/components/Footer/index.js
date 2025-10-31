99f760afea3624ed3057eed1c9ed4435ae5e2e6d
Madhav Ahuja <120156445+madhavahuja1@users.noreply.github.com>
2025-10-31 12:20:39 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Madhav Ahuja. All rights reserved.
+          &copy; 2025 Madhav Ahuja. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
